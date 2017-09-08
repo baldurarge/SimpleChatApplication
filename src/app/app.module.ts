dc5cbdafcba66a01e2a13915848796c51ccbbf4e
Baldurarge <baldurargesveins@gmail.com>
2017-09-08 10:34:07 +0200

    changed readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
+// FIREBASE CONFIG
 var config = {
     apiKey: "THEKEY",
     authDomain: "THEDOMAIN",
